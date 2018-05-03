a5d99a036da192f0c520d2fc4ebf6b4139025854
Arkhane <arkhane@okstate.edu>
2018-05-02 20:04:09 -0500

    CIS2013_Week15_Lab1
    CIS2013_Week15_Lab1

---
+++ Animal.cpp
@@ -0,0 +1,49 @@
+#include "Animal.h"
+
+Animal::Animal()
+{
+	set_legs(4);
+	set_alive(true);
+	set_species("Cat");
+}
+
+Animal::Animal(int legs, bool alive, string spec)
+{
+	number_of_legs = legs;
+	is_alive = alive;
+	species = spec;
+}
+
+Animal::~Animal()
+{
+	// Leaving Empty
+}
+
+void Animal::set_legs(int legs)
+{
+	number_of_legs = legs
+}
+int Animal::get_legs()
+{
+	return number_of_legs
+}
+void Animal::set_alive(bool alive)
+{
+	is_alive = alive;
+}
+bool Animal::get_alive()
+{
+	return is_alive;
+}
+void Animal::set_species(string spec)
+{
+	species = spec;
+}
+string Animal::get_species()
+{
+	return species;
+}
+
+string Animal::describe() {
+	return species << "with " << number_of_legss << "legs" << endl;
+}
